cba839f100e4370947b687d7b07f4c2be623f658
wayan29 <44164208+wayan29@users.noreply.github.com>
2019-03-04 17:00:49 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_704064403297977";
+$raw_uid="61c47a89-ba0d-4740-8f94-422c815e17be";
+$user_name="admin";
+$invite_code="F3DvRob3M1";
+$user_token="AbzJZk78zrjA1vZV";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="3059689615c642d6";
+$device_type="OPPO++CPH1729";
+
+
+?>
